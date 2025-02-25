5d1a014765a072525141868d0cec9490291f2491
Luca Mawyin <lucamawyin@gmail.com>
2025-02-25 09:56:30 -0500

    bug fixes

--- tetris/js/high-scores.js
+++ tetris/js/high-scores.js
@@ -2,6 +2,18 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    // Just making sure they go through the home screen
+    // Allows page to set up default values if they don't exist
+    if (!localStorage.getItem('visited'))
+    {
+        window.location.href("index.html");
+    }
+
+    localStorage.setItem('visited',true);
+
+
+    // Getting high scores and high score names from local storage
     let highScores = JSON.parse(localStorage.getItem('highScores'));
     let highNames = JSON.parse(localStorage.getItem('highNames'));
 
--- tetris/js/settings.js
+++ tetris/js/settings.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
+    
     // Default keybinds
     const defaultBind = {
         left: "a",
