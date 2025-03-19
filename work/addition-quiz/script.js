96323dbd6c85aec7bfb9149b65d47db456bd1d7d
LucaMawyin <lucamawyin@gmail.com>
2025-03-19 12:51:57 -0400

    Added lab work

--- work/addition-quiz/script.js
+++ work/addition-quiz/script.js
@@ -1,5 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelectorAll("input[type=number]").forEach(input => input.addEventListener("input", run));
+    document.querySelector("#operation").addEventListener("input",run);
+    run();
+});
+
 function run(){
     let op = document.querySelector("#operation").value;
+    
     let num1 = parseInt(document.querySelectorAll("input[type=number]")[0].value);
     let num2 = parseInt(document.querySelectorAll("input[type=number]")[1].value);
     let result = 0;
@@ -22,6 +30,5 @@ function run(){
             break;
     }
 
-    console.log(num1, num2)
-    console.log(result)
+    document.querySelectorAll("input[type=text]")[0].value = result;
 }
